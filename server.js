625ee63db5bce3884f827491090ae566909659c5
matiashidalgo11 <matiashidalgo18@gmail.com>
2021-11-19 16:01:03 -0300

    Primer Commit

---
+++ server.js
@@ -0,0 +1,114 @@
+/* import cors from "cors";
+import methodOverride from "method-override";
+import express, { response } from "express"; */
+
+const methodOverride = require("method-override");
+const cors = require("cors");
+const express = require("express");
+//
+
+const server = express();
+const log = console.log;
+
+let port = process.env.PORT || 3001;
+
+server.use(express.json());
+server.use(express.urlencoded({extended:true}));
+
+let users = [{email:"aux@aux", name:"aux",pass:"aux"},{email:"matiashidalgo18@gmail.com", name:"matias",pass:"1234"}];
+
+
+//GET
+server.get("/",(req, res) =>{
+    res.send("Server Funcionando");
+});
+
+server.get("/users", (req, res) => {
+    res.send(users);
+});
+
+server.get("/users/:email", (req, res) =>{
+    res.send(buscarPorEmail(req.params.email));
+});
+
+server.get('/name',(req, res) =>{
+
+    let nombre = req.query.nombre;
+    res.send(buscarPorName(nombre));
+
+});
+
+//POST
+
+server.post('/user', (req, res) =>{
+
+    let user = { email : req.body.email, name : req.body.name, pass : req.body.pass };
+    users.push(user);
+    res.send(users);
+});
+
+//DELETE
+
+server.delete('/user/:email', (req, res) => {
+
+    users = users.filter(elm => elm.email !== req.params.email);
+    res.send(users);
+
+});
+
+server.delete('/user', (req, res) => {
+
+    let emails = req.query.email;
+
+    let lengthReq = emails.length;
+
+    for(let i = 0; i < lengthReq; i++){
+
+        users = users.filter(elm => elm.email !== emails[i]);
+    }
+    
+
+    res.send(users);
+
+});
+
+//UPDATE
+
+server.put('/user/update',(req, res) => {
+
+    let email = req.body.email;
+
+    let index = users.findIndex(elm => elm.email === email);
+
+    let newUser = { email : email, name : req.body.name, pass : req.body.pass};
+
+    users[index] = newUser;
+
+    res.send(users);
+});
+
+
+//
+
+
+server.listen(port, ()=>{
+    log("start server");
+});
+
+
+//Funciones aux
+
+
+function buscarPorEmail(email){
+    
+    let user = users.filter(elm => elm.email === email);
+    
+    return user;
+}
+
+function buscarPorName(name){
+    
+    let user = users.filter(elm => elm.name === name);
+    
+    return user;
+}
